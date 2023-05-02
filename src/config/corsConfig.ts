b6fc429aab615ecdd4041ccfb9fda2d2e3ac7c71
Adrián Emanuel Koenig <adrian.95.koenig@gmail.com>
2023-05-02 13:38:42 -0300

    arreglos

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -1,5 +1,6 @@
 export const configCors = {
-    origin: process.env.URL_PERMISSION?.split(","),
+    // origin: process.env.URL_PERMISSION?.split(","),
+    origin: "*",
     methods: ["GET","POST","UPDATE","DELETE"],
     credentials: true
 }
